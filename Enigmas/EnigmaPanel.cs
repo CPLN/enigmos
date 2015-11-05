25a6c7785d9746bda1a293d3c13b4abe7e7a2477
Steeve Droz <steeve.droz@rpn.ch>
2015-11-05 12:17:08 +0100

    Suppression de méthode dépréciée.

--- Enigmas/EnigmaPanel.cs
+++ Enigmas/EnigmaPanel.cs
@@ -10,10 +10,5 @@ namespace Cpln.Enigmos.Enigmas
             BackColor = Color.White;
             Size = new Size(800, 600);
         }
-
-        protected void Center(Control element)
-        {
-            element.Location = new Point((Width - element.Width) / 2, (Height - element.Height) / 2);
-        }
     }
 }
