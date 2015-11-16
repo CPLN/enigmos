f4f6ec704092b7dd83c03d3f4eaebcfda46d4ed0
Steeve Droz <steeve.droz@rpn.ch>
2015-11-16 08:00:18 +0100

    Base de la classe Graph

---
+++ Enigmas/Components/Graph.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cpln.Enigmos.Enigmas.Components
+{
+    class Graph<T>
+    {
+        private Node root;
+
+        private class Node
+        {
+            private T element;
+            private List<Connection> connections = new List<Connection>();
+        }
+
+        private class Connection
+        {
+            private Node neighbor;
+            private Dictionary<string, object> tags = new Dictionary<string, object>();
+        }
+    }
+}
