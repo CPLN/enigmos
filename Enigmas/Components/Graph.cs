ede5f21ea882424058312481a6b00a94eec5c558
Steeve Droz <steeve.droz@rpn.ch>
2015-11-17 17:18:09 +0100

    Ajout de la méthode de distance
    La méthode de distance teste la distance entre deux noeuds d'un graph ou
    lève une exception s'il n'y a pas de connexion entre les deux noeuds.

--- Enigmas/Components/Graph.cs
+++ Enigmas/Components/Graph.cs
@@ -22,6 +22,73 @@ namespace Cpln.Enigmos.Enigmas.Components
                 Connections = new List<Connection>();
                 Tags = new Dictionary<string, object>();
             }
+
+            public int Distance(Node node)
+            {
+                string tagVisited = "__distance__visited__";
+                string tagParent = "__distance__parent__";
+                string tagDistance = "__distance__distance__";
+
+                int distance = -1;
+
+                List<Node> nodesToVisit = new List<Node>();
+                List<Node> visitedNodes = new List<Node>();
+
+                this.Tags.Add(tagParent, null);
+                this.Tags.Add(tagDistance, 0);
+                nodesToVisit.Add(this);
+
+                while (nodesToVisit.Count > 0)
+                {
+                    Node current = nodesToVisit[0];
+                    nodesToVisit.RemoveAt(0);
+
+                    if (current.Tags.ContainsKey(tagVisited))
+                    {
+                        continue;
+                    }
+
+                    current.Tags.Add(tagVisited, true);
+
+                    foreach (Connection connection in current.Connections)
+                    {
+                        Node neighbor = connection.Neighbor;
+                        if (neighbor.Tags.ContainsKey(tagParent))
+                        {
+                            if ((int)neighbor.Tags[tagParent] - 1 > (int)current.Tags[tagParent])
+                            {
+                                neighbor.Tags.Add(tagParent, current);
+                                neighbor.Tags.Add(tagDistance, (int)current.Tags[tagDistance] + 1);
+                            }
+                        }
+                        else
+                        {
+                            neighbor.Tags.Add(tagParent, current);
+                            neighbor.Tags.Add(tagDistance, (int)current.Tags[tagDistance] + 1);
+                            nodesToVisit.Add(neighbor);
+                        }
+                    }
+                }
+
+                if (node.Tags.ContainsKey(tagVisited))
+                {
+                    distance = (int)node.Tags[tagDistance];
+                }
+
+                foreach (Node visitedNode in visitedNodes)
+                {
+                    visitedNode.Tags.Remove(tagVisited);
+                    visitedNode.Tags.Remove(tagParent);
+                    visitedNode.Tags.Remove(tagDistance);
+                }
+
+                if (distance < 0)
+                {
+                    throw new NotConnectedException(this, node);
+                }
+
+                return distance;
+            }
         }
 
         private class Connection
@@ -33,6 +100,24 @@ namespace Cpln.Enigmos.Enigmas.Components
             {
                 this.Neighbor = neighbor;
                 this.Tags = new Dictionary<string, object>();
+            }
+        }
+
+        public class NotConnectedException : Exception
+        {
+            private Node origin;
+            private Node destination;
+
+            public string Message {
+                get {
+                    return "Les noeuds " + origin + " et " + destination + " ne sont pas connectés";
+                }
+            }
+
+            public NotConnectedException(Node origin, Node destiation)
+            {
+                this.origin = origin;
+                this.destination = destiation;
             }
         }
     }
