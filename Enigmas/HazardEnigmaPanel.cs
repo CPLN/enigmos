31beac17dd637a796e8343337c866c0832e2732a
Knöpfel Sacha <KnoepfelS@s2.rpn.ch>
2015-11-19 11:41:54 +0100

    Création de l'envoi de la reponse au moment du clique du bon bouton.

--- Enigmas/HazardEnigmaPanel.cs
+++ Enigmas/HazardEnigmaPanel.cs
@@ -10,12 +10,29 @@ namespace Cpln.Enigmos.Enigmas
 {
     public class HazardEnigmaPanel : EnigmaPanel
     {
+        private int iBonBouton;
+        private Button[] btnReponse = new Button[4];
         public HazardEnigmaPanel()
         {
-            Button[] btnReponse = new Button[4];
+            Random randoChoixDuBouton = new Random();
+            iBonBouton = randoChoixDuBouton.Next(0, 4);
+
             for (int i = 0; i < 4; i++)
             {
                 btnReponse[i] = new Button();
+                btnReponse[i].Width = 390;
+                btnReponse[i].Height = 290;
+                btnReponse[i].Text = "Bonne réponse?";
+                btnReponse[i].FlatStyle = FlatStyle.System;
+                Controls.Add(btnReponse[i]);
+                if(i == iBonBouton)
+                {
+                    btnReponse[iBonBouton].Click += new EventHandler(BonneReponse);
+                }
+                else
+                {
+                    btnReponse[i].Click += new EventHandler(MauvaiseReponse);
+                }
             }
 
 
@@ -23,14 +40,23 @@ namespace Cpln.Enigmos.Enigmas
             btnReponse[1].Location = new Point(410, 0);
             btnReponse[2].Location = new Point(0, 310);
             btnReponse[3].Location = new Point(410, 310);
-            for (int i = 0; i < 4; i++)
+
+        }
+        private void BonneReponse(object sender, EventArgs e)
+        {
+            MessageBox.Show("La reponse est suivant.", "Reponse");
+           /* for (int i = 0; i < 4; i++)
             {
-                btnReponse[i].Width = 390;
-                btnReponse[i].Height = 290;
-                btnReponse[i].Text = "Bonne réponse?";
-                btnReponse[i].FlatStyle = FlatStyle.System;
-                Controls.Add(btnReponse[i]);
+                btnReponse[i].Enabled = false;
             }
+            */
+
+        }
+
+        private void MauvaiseReponse(object sender, EventArgs e)
+        {
+            MessageBox.Show("Vous avez cliqué sur un mauvais bouton.", "Mauvais Bouton");
+
         }
     }
 }
