7f52d652fcb061af73abb5e1ba29dda4ed8be108
El-Sheikh Mohamed <ElSheikhMo@s2.rpn.ch>
2015-11-19 09:51:52 +0100

    Test du code

--- Enigmas/InstinctEnigmaPanel.cs
+++ Enigmas/InstinctEnigmaPanel.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace Cpln.Enigmos.Enigmas
 {
     public class InstinctEnigmaPanel : EnigmaPanel
     {
+        public InstinctEnigmaPanel()
+        {
+            Label lblEnigme = new Label();
+
+            lblEnigme.Text = "Coucou";
+            lblEnigme.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
+            lblEnigme.Dock = DockStyle.Fill;
+            lblEnigme.TextAlign = ContentAlignment.MiddleCenter;
+
+            Controls.Add(lblEnigme);
+        }
+     
     }
 }
