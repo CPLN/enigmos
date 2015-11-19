c3f8f47ddbf2a0855989b087cfc2760819fdcab2
Bosch Arnaud <BoschAr@s2.rpn.ch>
2015-11-19 10:29:03 +0100

    Ajout d'un nouvel élément du jeu (PictureBox du joueur)

--- Enigmas/RunEnigmaPanel.cs
+++ Enigmas/RunEnigmaPanel.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace Cpln.Enigmos.Enigmas
 {
@@ -10,6 +7,14 @@ namespace Cpln.Enigmos.Enigmas
     {
         public RunEnigmaPanel()
         {
+            //Création des composants
+            PictureBox pbxHomme = new PictureBox();
+            pbxHomme.Size = new Size(25, 50);
+            pbxHomme.BackColor = Color.Red;
+            pbxHomme.Location = new Point(400 - pbxHomme.Width,575 - pbxHomme.Height);
+
+            //Ajout des éléments
+            Controls.Add(pbxHomme);
 
         }
     }
