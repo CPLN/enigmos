048a5cfe2265a4e0b6e4458e3c7196c10f20f44d
Gümpel Rémy <GuempelR@s2.rpn.ch>
2015-11-19 09:46:35 +0100

    Ajout d'une nouvelle énigme (class).

---
+++ Enigmas/BeatThemAllEnigmaPanel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Cpln.Enigmos.Enigmas
+{
+    /// <summary>
+    /// Exemple d'énigme très simple. Seul un texte est affiché.
+    /// </summary>
+    public class BeatThemAllEnigmaPanel : EnigmaPanel
+    {
+        /// <summary>
+        /// Constructeur par défaut, génère un texte et l'affiche dans le Panel.
+        /// </summary>
+        public BeatThemAllEnigmaPanel()
+        {
+            /*Label lblEnigme = new Label();
+
+            lblEnigme.Text = "Test";
+            lblEnigme.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
+            lblEnigme.Dock = DockStyle.Fill;
+            lblEnigme.TextAlign = ContentAlignment.MiddleCenter;
+
+            Controls.Add(lblEnigme);*/
+        }
+    }
+}
