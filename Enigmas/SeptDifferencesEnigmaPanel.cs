3c587c31f1145274aaa377db65fc5b60739069af
Chopard Dimitri <ChopardDi@s2.rpn.ch>
2015-11-19 11:55:39 +0100

    Amélioration code Création méthode pour création de panel

--- Enigmas/SeptDifferencesEnigmaPanel.cs
+++ Enigmas/SeptDifferencesEnigmaPanel.cs
@@ -12,8 +12,10 @@ namespace Cpln.Enigmos.Enigmas
     {
         public SeptDifferencesEnigmaPanel()
         {
+
             //Elargissement de la form
             this.Width = this.Width + 100;
+
             //Création de la première PictureBox contenant la première image
             PictureBox Img1 = new PictureBox();
             Img1.BackgroundImage = Properties.Resources.SeptDifference1;
@@ -21,6 +23,7 @@ namespace Cpln.Enigmos.Enigmas
             Img1.Height = Properties.Resources.SeptDifference1.Height;
             Img1.Location = new Point(20,200);
             Controls.Add(Img1);
+
             //Création de la 2ème PictureBox contenant la 2ème image
             PictureBox Img2 = new PictureBox();
             Img2.BackgroundImage = Properties.Resources.SeptDifference2;
@@ -28,14 +31,22 @@ namespace Cpln.Enigmos.Enigmas
             Img2.Height = Properties.Resources.SeptDifference2.Height;
             Img2.Location = new Point(485,200);
             Controls.Add(Img2);
-            Label lblEnigme = new Label();
 
+            //Création du titre(donnée)
+            Label lblEnigme = new Label();
             lblEnigme.Text = "Cherchez les 7 différences dans l'image de droite";
             lblEnigme.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
             lblEnigme.Dock = DockStyle.Fill;
             lblEnigme.TextAlign = ContentAlignment.TopCenter;
 
             Controls.Add(lblEnigme);
+            
+            public void CreatePnl()
+            {
+
+            }
+
+            
         }
     }
 }
