19a055b61acace211a27d4b568acb0973496bb7e
Bosch Arnaud <BoschAr@s2.rpn.ch>
2015-11-19 12:04:10 +0100

    Création d'une fonction pour le placement des obstacles en plus de quelque condition et une variable random pour les obstacles

--- Enigmas/RunEnigmaPanel.cs
+++ Enigmas/RunEnigmaPanel.cs
@@ -7,11 +7,12 @@ namespace Cpln.Enigmos.Enigmas
     class RunEnigmaPanel : EnigmaPanel
     {
         private Timer timer= new Timer();
+        //Déclaration de variable
+        PictureBox[] tblObstacle = new PictureBox[2];
+        int iCompteur = 0;
+        Random random = new Random();
         public RunEnigmaPanel()
         {
-            //Déclaration des variables
-            PictureBox[] tblObstacle = new PictureBox[2];
-            
             //Création du joueur
             PictureBox pbxHomme = new PictureBox();
             pbxHomme.Size = new Size(25, 50);
@@ -27,16 +28,16 @@ namespace Cpln.Enigmos.Enigmas
             PictureBox pbxCaillou = new PictureBox();
             pbxCaillou.Size = new Size(50, 50);
             pbxCaillou.BackColor = Color.Gray;
-            pbxCaillou.Location = new Point(400 - pbxCaillou.Width, 75 - pbxCaillou.Height);
+            tblObstacle[0] = pbxCaillou;
             PictureBox pbxTronc = new PictureBox();
             pbxTronc.Size = new Size(75, 50);
             pbxTronc.BackColor = Color.Green;
-            pbxTronc.Location = new Point(400 - pbxTronc.Width, 100 - pbxTronc.Height);
-
-            //Tableau
-            tblObstacle[0] = pbxCaillou;
             tblObstacle[1] = pbxTronc;
-            
+
+            //Appel de la fonction pour le placement de l'obstacle
+            PlacementObstacle(pbxTronc);
+            PlacementObstacle(pbxCaillou);
+       
             //Ajout des éléments
             Controls.Add(pbxHomme);
             Controls.Add(pbxCaillou);
@@ -46,7 +47,48 @@ namespace Cpln.Enigmos.Enigmas
 
         private void Timer_Tick(object sender, EventArgs e)
         {
+            iCompteur += 1;
+            tblObstacle[0].Top += 5;
+            tblObstacle[1].Top += 5;
 
+            //Condition pour que les obstacles tombent en boucle
+            if(tblObstacle[0].Top >= 600 && tblObstacle[1].Top >= 600)
+            {
+                PlacementObstacle(tblObstacle[0]);
+                PlacementObstacle(tblObstacle[1]);
+            }
+        }
+
+        //Fonction pour placer les obstacles sur les différents chemin 
+        private void PlacementObstacle(PictureBox pbxObstacle)
+        {
+            //random pour qu'il choisisse le chemin aléatoirement
+            switch (random.Next(3))
+            {
+                case 0:
+                    {
+                        pbxObstacle.Left = 150;
+                        pbxObstacle.Top = 0 - pbxObstacle.Height;
+                    }
+                    break;
+                case 1:
+                    {
+                        pbxObstacle.Left = 300;
+                        pbxObstacle.Top = 0 - pbxObstacle.Height;
+                    }
+                    break;
+                case 2:
+                    {
+                        pbxObstacle.Left = 450;
+                        pbxObstacle.Top = 0 - pbxObstacle.Height;
+                    }
+                    break;
+            }
+            //Condition pour que les obstacles ne soient jamais à la même place
+            if(tblObstacle[0].Left == tblObstacle[1].Left)
+            {
+                PlacementObstacle(pbxObstacle);
+            }
         }
     }
 }
