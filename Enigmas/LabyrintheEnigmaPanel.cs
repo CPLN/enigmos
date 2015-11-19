5366aa5bd4db1e986e3df5f2a030a8c4a71793f5
Chiffelle Janis <ChiffelleJ@s2.rpn.ch>
2015-11-19 11:50:30 +0100

    link du Graph avec le programme

--- Enigmas/LabyrintheEnigmaPanel.cs
+++ Enigmas/LabyrintheEnigmaPanel.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Cpln.Enigmos.Enigmas.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Cpln.Enigmos.Enigmas
 {
@@ -11,6 +13,9 @@ namespace Cpln.Enigmos.Enigmas
     /// </summary>
     public class LabyrintheEnigmaPanel : EnigmaPanel
     {
-
+        public LabyrintheEnigmaPanel()
+        {
+            Graph<Panel> graph = new Graph<Panel>(new Panel());
+        }
     }
 }
