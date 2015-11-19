30e6c8bf448596331f2c7e0d808ea8d9b0253662
Bosch Arnaud <BoschAr@s2.rpn.ch>
2015-11-19 09:43:21 +0100

    Ajout d'une nouvelle énigme (Roux run)

---
+++ Enigmas/RunEnigmaPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cpln.Enigmos.Enigmas
+{
+    class RunEnigmaPanel : EnigmaPanel
+    {
+        public RunEnigmaPanel()
+        {
+
+        }
+    }
+}
