9205da5197d6a188d37a0fb1a48262a63b51cf49
Bosch Arnaud <BoschAr@s2.rpn.ch>
2015-11-19 10:32:56 +0100

    Ajout d'un premier obstacle

--- Enigmas/RunEnigmaPanel.cs
+++ Enigmas/RunEnigmaPanel.cs
@@ -7,14 +7,21 @@ namespace Cpln.Enigmos.Enigmas
     {
         public RunEnigmaPanel()
         {
-            //Création des composants
+            //Création du joueur
             PictureBox pbxHomme = new PictureBox();
             pbxHomme.Size = new Size(25, 50);
             pbxHomme.BackColor = Color.Red;
             pbxHomme.Location = new Point(400 - pbxHomme.Width,575 - pbxHomme.Height);
 
+            //Création d'un obstacle
+            PictureBox pbxCaillou = new PictureBox();
+            pbxCaillou.Size = new Size(50, 50);
+            pbxCaillou.BackColor = Color.Gray;
+            pbxCaillou.Location = new Point(400 - pbxCaillou.Width, 75 - pbxCaillou.Height);
+
             //Ajout des éléments
             Controls.Add(pbxHomme);
+            Controls.Add(pbxCaillou);
 
         }
     }
