c384b8f8a48a229c02d85a5eebbd73646172259b
Harun Bahi <HarunBa@s2.rpn.ch>
2015-11-19 11:20:47 +0100

    J'ai crée les boutons, et cristiano

--- Enigmas/TrouverEnigmaPanel.cs
+++ Enigmas/TrouverEnigmaPanel.cs
@@ -16,13 +16,39 @@ namespace Cpln.Enigmos.Enigmas
         public TrouverEnigmaPanel()
         {
             Label lblEnigme = new Label();
-
-            lblEnigme.Text = "fesse ?";
+            int i = 0;
+            Random random = new Random();
+            for (i = 0; i < 100; i++)
+            {
+                Button b = new Button();
+                b.Text = "" + i;
+                b.Size = new Size(100, 100);
+                b.Location = new Point(random.Next(800), random.Next(600));
+                i++;
+                Controls.Add(b);
+            }
+            Button bCristiano = new Button();
+            bCristiano.Text = "" + 666;
+            bCristiano.Size = new Size(100, 100);
+            bCristiano.Location = new Point(random.Next(800), random.Next(600));
+            bCristiano.BackColor = Color.Red;
+            bCristiano.Click += new EventHandler(bCristiano_Click);
+            Controls.Add(bCristiano);
             lblEnigme.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
             lblEnigme.Dock = DockStyle.Fill;
             lblEnigme.TextAlign = ContentAlignment.MiddleCenter;
-
             Controls.Add(lblEnigme);
+            }     
+            private void bCristiano_Click(object sender, EventArgs e)
+            {
+            MessageBox.Show("Le mot à valider est cristiano", "Yo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+
+
+            //lblEnigme.Text = "fesse ?";
+            
+            
         }
     }
-}
+
