80340ba8603913968f887f995c69afad02a3b3a2
Loison Yohann <LoisonYo@s2.rpn.ch>
2015-11-19 10:56:46 +0100

    Déclaration des poissons et ajout dans un tableau

--- Enigmas/PhoqueEnigmaPanel.cs
+++ Enigmas/PhoqueEnigmaPanel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Drawing;
 using System.Windows.Forms;
+using System;
 
 namespace Cpln.Enigmos.Enigmas
 {
@@ -7,14 +8,29 @@ namespace Cpln.Enigmos.Enigmas
     {
         public PhoqueEnigmaPanel()
         {
+            //Déclaration des variables et tableaux
+            PictureBox[] tblPoissons = new PictureBox[3];
+            Random RandomX = new Random();
+
             //Création du Phoque
             PictureBox pbxPhoque = new PictureBox();
             pbxPhoque.Size = new Size(20, 40);
             pbxPhoque.BackColor = Color.Blue;
             pbxPhoque.Location = new Point(400 - (pbxPhoque.Width / 2), 550 - (pbxPhoque.Height));
-
-            //Ajout des composants
             Controls.Add(pbxPhoque);
+
+            //Création des poissons
+            for (int i = 0; i < tblPoissons.Length; i++)
+            {
+                PictureBox pbxPoisson = new PictureBox();
+                pbxPoisson.Size = new Size(20, 40);
+                pbxPoisson.BackColor = Color.Gray;
+                pbxPoisson.Location = new Point(RandomX.Next(0, 800) - pbxPoisson.Width, 0 - pbxPoisson.Height);
+                pbxPoisson.Name = "Poisson_" + i;
+                tblPoissons[i] = pbxPoisson;
+                Controls.Add(pbxPoisson);
+            }
+
         }
     }
 }
