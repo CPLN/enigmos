3de01b25af12fda27280c027b86dbaba00524df3
Loison Yohann <LoisonYo@s2.rpn.ch>
2015-11-19 09:43:17 +0100

    Ajout d'une nouvelle éngime (Chop' les poissons)

---
+++ Enigmas/PhoqueEnigmaPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cpln.Enigmos.Enigmas
+{
+    class PhoqueEnigmaPanel : EnigmaPanel
+    {
+        public PhoqueEnigmaPanel()
+        {
+
+        }
+    }
+}
