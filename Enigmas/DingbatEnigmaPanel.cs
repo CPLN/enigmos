abeba92704d2ca043ed152ba145c7e58003f4128
Buzala Adrian <BuzalaAd@s2.rpn.ch>
2015-11-25 16:05:46 +0100

    Fin de la première énigme

--- Enigmas/DingbatEnigmaPanel.cs
+++ Enigmas/DingbatEnigmaPanel.cs
@@ -13,16 +13,20 @@ namespace Cpln.Enigmos.Enigmas
     {
         public DingbatEnigmaPanel()
         {
+            Random rnd = new Random();
             for(int i = 0; i < 6; i++)
             {
+                int x = rnd.Next(1, 700);
+                int y = rnd.Next(1, 550);
                 Label lblCitronVert = new Label();
 
                 lblCitronVert.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
                 lblCitronVert.ForeColor = Color.Green;
-                lblCitronVert.Dock = DockStyle.Fill;
-                lblCitronVert.Location = new Point(0, 0);
+                lblCitronVert.Location = new Point(x, y);
                 lblCitronVert.Text = "Tronc";
-                
+                lblCitronVert.AutoSize = false;
+
+                lblCitronVert.Size = TextRenderer.MeasureText(lblCitronVert.Text, lblCitronVert.Font);
                 Controls.Add(lblCitronVert);
             }
         }
