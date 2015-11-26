6e01b29af1264b88b36189f6d1a80c61dbfc676a
Harun Bahi <HarunBa@s2.rpn.ch>
2015-11-26 12:12:13 +0100

    Amélioration

--- Enigmas/TrouverEnigmaPanel.cs
+++ Enigmas/TrouverEnigmaPanel.cs
@@ -13,27 +13,31 @@ namespace Cpln.Enigmos.Enigmas
         /// <summary>
         /// Constructeur par défaut, génère un texte et l'affiche dans le Panel.
         /// </summary>
-        bool bGo = false, bRebondX = true, bRebondY = true;
-        int iAxeX = 3, iAxeY = 2;
+        bool bGo = false, bRebondX = true, bRebondY = true, bRebondXC = true, bRebondYC = true;
+        int iAxeX = 4, iAxeY = 2;
         private Timer Timer = new Timer();
-        Button b = new Button();
         Button bCristiano = new Button();
+        List<Button> buttons = new List<Button>();
         public TrouverEnigmaPanel()
         {
-
+            //bGo = true;
             Label lblEnigme = new Label();
             int i = 0;
             Random random = new Random();
-            for (i = 0; i < 100; i++)
+            for (i = 0; i < 5; i++)
             {
-                
+
+                Button b = new Button();
+                buttons.Add(b);
+                this.Controls.Add(b);
                 b.Text = "" + i;
                 b.Size = new Size(100, 100);
                 b.Location = new Point(random.Next(800), random.Next(600));
-                i++;
+                b.Name = "Cristiano" + i;
                 Controls.Add(b);
+                
             }
-            
+
             bCristiano.Text = "" + 666;
             bCristiano.Size = new Size(100, 100);
             bCristiano.Location = new Point(random.Next(800), random.Next(600));
@@ -49,11 +53,11 @@ namespace Cpln.Enigmos.Enigmas
             Timer.Interval = 1; // 1 milliseconde
             Timer.Tick += new EventHandler(Timer_Tick);
             Timer.Start();
-
+            bGo = true;
 
             //lblEnigme.Text = "fesse ?";
-            bGo = true;
-            
+
+
 
         }
         private void bCristiano_Click(object sender, EventArgs e)
@@ -65,8 +69,44 @@ namespace Cpln.Enigmos.Enigmas
 
         private void Timer_Tick(object sender, EventArgs e)
         {
+            foreach (Button b in buttons)
+            {
+                Deplacement(b);
+            }
+            
+            if (bGo == true)
+                {
+                    if (bCristiano.Left <= 0 || bCristiano.Right >= this.Width)
+                    {
+                        bRebondXC = !bRebondXC;
+                    }
+                    if (bCristiano.Top <= 0 || bCristiano.Bottom >= this.Height)
+                    {
+                        bRebondYC = !bRebondYC;
+                    }
+                    if (bRebondYC == true)
+                    {
+                        bCristiano.Top += iAxeY;
+                    }
+                    else
+                    {
+                        bCristiano.Top -= iAxeY;
+                    }
+                    if (bRebondXC == true)
+                    {
+                        bCristiano.Left += iAxeX;
+                    }
+                    else
+                    {
+                        bCristiano.Left -= iAxeX;
+                    }
+                }
+            }
+        public void Deplacement(Button b)
+        {
             if (bGo == true)
             {
+
                 if (b.Left <= 0 || b.Right >= this.Width)
                 {
                     bRebondX = !bRebondX;
@@ -91,8 +131,10 @@ namespace Cpln.Enigmos.Enigmas
                 {
                     b.Left -= iAxeX;
                 }
+
             }
         }
-       }
-    }
+     }
+ }
+
 
