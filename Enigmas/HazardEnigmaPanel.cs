3e2984ac0c885ce3c94c379d6df396c8ef995f62
Knöpfel Sacha <KnoepfelS@s2.rpn.ch>
2015-11-26 10:24:33 +0100

    Fin du projet "Le Hazard"

--- Enigmas/HazardEnigmaPanel.cs
+++ Enigmas/HazardEnigmaPanel.cs
@@ -11,35 +11,38 @@ namespace Cpln.Enigmos.Enigmas
     public class HazardEnigmaPanel : EnigmaPanel
     {
         private int iBonBouton;
+        //Création des boutons__________________________________________________________
         private Button[] btnReponse = new Button[4];
+        Random randoChoixDuBouton = new Random();
         public HazardEnigmaPanel()
         {
-            Random randoChoixDuBouton = new Random();
-            iBonBouton = randoChoixDuBouton.Next(0, 4);
-
-            for (int i = 0; i < 4; i++)
-            {
-                btnReponse[i] = new Button();
-            }
             CreationBouton();
-
-            btnReponse[0].Location = new Point(0, 0);
-            btnReponse[1].Location = new Point(410, 0);
-            btnReponse[2].Location = new Point(0, 310);
-            btnReponse[3].Location = new Point(410, 310);
-
         }
 
         private void CreationBouton()
         {
+            SuspendLayout();
+            iBonBouton = randoChoixDuBouton.Next(0, 4);//Choix du bouton qui donnera la bonne réponse.
             for (int i = 0; i < 4; i++)
             {
-                btnReponse[i].Width = 390;
+                Controls.Remove(btnReponse[i]);
+                btnReponse[i] = new Button();
+            }
+
+            btnReponse[0].Location = new Point(0, 0);//Placement des boutons
+            btnReponse[1].Location = new Point(410, 0);
+            btnReponse[2].Location = new Point(0, 310);
+            btnReponse[3].Location = new Point(410, 310);
+
+            for (int i = 0; i < 4; i++)
+            {
+                btnReponse[i].Width = 390;//attribution d'une taille et d'un texte pour les boutons
                 btnReponse[i].Height = 290;
                 btnReponse[i].Text = "Bonne réponse?";
                 btnReponse[i].Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
                 btnReponse[i].FlatStyle = FlatStyle.System;
                 Controls.Add(btnReponse[i]);
+
                 if (i == iBonBouton)
                 {
                     btnReponse[iBonBouton].Click += new EventHandler(BonneReponse);
@@ -49,23 +52,23 @@ namespace Cpln.Enigmos.Enigmas
                     btnReponse[i].Click += new EventHandler(MauvaiseReponse);
                 }
             }
+            ResumeLayout();
+            //Fin de la création des boutons_______________________________________________________________
         }
         private void BonneReponse(object sender, EventArgs e)
         {
-            MessageBox.Show("La reponse est suivant.", "Reponse");
-           /* for (int i = 0; i < 4; i++)
+            MessageBox.Show("La reponse est suivant.", "Reponse");//Affichage de la reponse.
+            for (int i = 0; i < 4; i++)
             {
                 btnReponse[i].Enabled = false;
             }
-            */
+            
 
         }
 
         private void MauvaiseReponse(object sender, EventArgs e)
         {
-            MessageBox.Show("Vous avez cliqué sur un mauvais bouton.", "Mauvais Bouton");
-            Random randoChoixDuBouton = new Random();
-            iBonBouton = randoChoixDuBouton.Next(0, 4);
+            MessageBox.Show("Vous avez cliqué sur un mauvais bouton.", "Mauvais Bouton");//affichage du clique sur un mauvais boutons
             CreationBouton();
         }
     }
