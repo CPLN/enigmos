d62d20c6a2c07b8f0025c84b41f073379db77483
El-Sheikh Mohamed <ElSheikhMo@s2.rpn.ch>
2015-11-26 10:34:14 +0100

    Ajout d'evenemement de clique

--- Enigmas/InstinctEnigmaPanel.cs
+++ Enigmas/InstinctEnigmaPanel.cs
@@ -1,32 +1,73 @@
 ﻿using System.Drawing;
 using System.Windows.Forms;
+using System;
+using System.Timers;
+using Cpln.Enigmos.Exceptions;
+using Cpln.Enigmos.Utils;
+
+using System.Collections.Generic;
 
 namespace Cpln.Enigmos.Enigmas
 {
+       
     public class InstinctEnigmaPanel : EnigmaPanel
     {
+                private void btnChoix1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Le mot à valider est cristiano", "Yo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+       
+     
         public InstinctEnigmaPanel()
         {
            Label lblReponse = new Label();
             Label lblTexte = new Label();
-
             Panel pnlImage = new Panel();
-
-
-
-            lblReponse.Text = "Réponse A : \n" + "Réponse B : \n" + "Réponse C : \n";
-            lblReponse.Font = new Font(FontFamily.GenericSansSerif, 12, FontStyle.Bold);
-            lblReponse.Size = new Size(150, 70);
-            lblReponse.Location = new Point(0, 0);
-           
-           
-            
-
-
+            Button btnChoix1 = new Button();
+            Button btnChoix2 = new Button();
+            Button btnChoix3 = new Button();
+            bool bFindeJeu = false;
             lblTexte.Text = "Vous vous réveillez dans une baignoire.";
             lblTexte.Font = new Font(FontFamily.GenericSansSerif, 12, FontStyle.Bold);
             lblTexte.Size = new Size(400, 30);
             lblTexte.Location = new Point(470, 0);
+            btnChoix1.Location = new Point(200, 200);
+            btnChoix2.Location = new Point(320, 100);
+            btnChoix3.Location = new Point(400, 200);
+            Controls.Add(lblReponse);
+            Controls.Add(lblTexte);
+            Controls.Add(btnChoix1);
+            Controls.Add(btnChoix2);
+            Controls.Add(btnChoix3);
+            pnlImage.Width = 800;
+            pnlImage.Height = 600;
+            //pnlImage.BackColor = Color.BlueViolet;
+
+            pnlImage.BackgroundImage = Properties.Resources.depart;
+            Controls.Add(pnlImage);
+
+            btnChoix1.Click += new EventHandler(btnChoix1_Click);
+        
+  
+    
+
+
+            /*lblReponse.Text = "Réponse A : \n" + "Réponse B : \n" + "Réponse C : \n";
+            lblReponse.Font = new Font(FontFamily.GenericSansSerif, 12, FontStyle.Bold);
+            lblReponse.Size = new Size(150, 70);
+            lblReponse.Location = new Point(0, 0);*/
+           
+           /*while (!bFindeJeu)
+           {
+           
+
+
+
+           }*/
+            
+
+
+
          
             
             
@@ -41,6 +82,14 @@ namespace Cpln.Enigmos.Enigmas
 
             pnlImage.BackgroundImage = Properties.Resources.depart;
             Controls.Add(pnlImage);
+
+           // Image[] images = new Image[2]{Properties.Resources.depart, Properties.Resources.depart, Properties.Resources.depart };
+
+         
+            
+            
+
+            
             
 
 
