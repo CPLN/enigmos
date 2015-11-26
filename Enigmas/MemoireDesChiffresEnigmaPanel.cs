d3b6998edcd45467ca9ce8a9782e66a84b6fd560
Jeanneret Hugo (CPLN - ET) <JeanneretH2@s2.rpn.ch>
2015-11-26 11:45:33 +0100

    Création du bouton recommencer.

--- Enigmas/MemoireDesChiffresEnigmaPanel.cs
+++ Enigmas/MemoireDesChiffresEnigmaPanel.cs
@@ -18,9 +18,11 @@ namespace Cpln.Enigmos.Enigmas
         Random random = new Random(); //Déclaration du générateur de random
         int iCaseDemandee; //Déclaration de la case demandée
 
+        Button btnRecommencer = new Button();
+
         public MemoireDesChiffresEnigmaPanel()
         {
-            iCaseDemandee = 1;//random.Next(1, 4); //Affectation de la case demandée
+            iCaseDemandee = random.Next(1, 4); //Affectation de la case demandée
 
             //Mise en place d'un timer
             timer.Interval = 500; // 1 demi secondes
@@ -33,6 +35,15 @@ namespace Cpln.Enigmos.Enigmas
             lblQuestion.Font = new Font("Verdana", 40);
             lblQuestion.Location = new Point(50, 400);
             lblQuestion.Visible = false;
+
+            //Caractéristiques du bouton
+            Controls.Add(btnRecommencer);
+            btnRecommencer.Visible = true;
+            btnRecommencer.Size = new Size(100, 50);
+            btnRecommencer.Click += new EventHandler(btnRecommencer_click);
+            btnRecommencer.Text = "Recommencer";
+            btnRecommencer.Location = new Point(337, 500);
+            btnRecommencer.Visible = false;
             
             //Caractéristiques du label qui fait apparaître les chiffres aléatoires
             Controls.Add(lblChiffres);
@@ -42,6 +53,7 @@ namespace Cpln.Enigmos.Enigmas
         }
         void Timer_Tick(object sender, EventArgs e)
         {
+            lblChiffres.Visible = true;
             if (iCompteur <= 7) //Si le tableau n'est pas rempli
             {
                 if (iCompteur != iCaseDemandee) //S'il ne s'agit pas de la case qui sera demandée
@@ -62,17 +74,29 @@ namespace Cpln.Enigmos.Enigmas
                     tChiffresAleatoire[iCompteur] = 8; //La case demandée sera forcément 8
                 }
 
-                lblChiffres.Text = Convert.ToString(tChiffresAleatoire[iCompteur]);
+                lblChiffres.Text = Convert.ToString(tChiffresAleatoire[iCompteur]); //Le chiffre aléatoire est écrit dans le label
                 iCompteur++;
             }
-            else if(iCompteur == 8)
+            else if(iCompteur == 8) //Quand le tableau est rempli
             {
-                lblQuestion.Visible = true;
+                lblQuestion.Visible = true; //La question deviens visible
                 iCaseDemandee++; //Il faut l'incrémenter de un car il part à 1 alors que le tableau par a 0
-                lblQuestion.Text = String.Format("Quel est le {0}ème chiffre", iCaseDemandee);
+                lblQuestion.Text = String.Format("Quel est le {0}ème chiffre", iCaseDemandee); //Contenu de la question
                 iCompteur = 0;
-                timer.Stop();
+                timer.Stop(); //Fin du timer
+                btnRecommencer.Visible = true;
             }
+        }
+
+        void btnRecommencer_click(object sender, EventArgs e)
+        {
+            //Remise a zero des variables et cache des labels
+            iCompteur = 0;
+            btnRecommencer.Visible = false;
+            iCaseDemandee = random.Next(1, 4);
+            lblQuestion.Visible = false;
+            lblChiffres.Visible = false;
+            timer.Start();
         }
     }
 }
