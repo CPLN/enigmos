8b4b3c88e88a605a011691117e2caab2dd5b02df
Chiffelle Janis <ChiffelleJ@s2.rpn.ch>
2015-11-26 12:13:05 +0100

    declaration des listes et mise en commentaire des testes

--- Enigmas/LabyrintheEnigmaPanel.cs
+++ Enigmas/LabyrintheEnigmaPanel.cs
@@ -14,7 +14,7 @@ namespace Cpln.Enigmos.Enigmas
     /// </summary>
     public class LabyrintheEnigmaPanel : EnigmaPanel
     {
-
+        /* test
         List<Panel> Case;
 
         private void test()
@@ -26,18 +26,31 @@ namespace Cpln.Enigmos.Enigmas
             test.Height = 50;
             test.BackColor = Color.Red;
             this.Controls.Add(test);
-        }
+        }*/
+
+        List<Panel> zonelaby;
+        List<Panel> mur;
 
         public LabyrintheEnigmaPanel()
         {
             Graph<Panel> graph = new Graph<Panel>(new Panel());
 
+            /* test
             Case = new List<Panel>();
             test();
             graph.Root.Element = Case[0];
             graph.Root.FindNeighbor(Case[0]);
             graph.Root.AddNeighbor(Case[0]);
-            
+            if (graph.Root.Distance(graph.Contains(Case[0])) == -1)
+            {
+                
+            }
+            graph.Contains(Case[0]).AddNeighbor(Case[1]);*/
+
+            zonelaby = new List<Panel>();
+            mur = new List<Panel>();
+
+
         }
     }
 }
