32e4dd601198d6ab3e5e38326d87caccdf97fd1d
Steeve Droz <steeve.droz@rpn.ch>
2015-12-03 10:27:12 +0100

    Nettoyage avant ajout dans le projet

--- EnigmaReferencer.cs
+++ EnigmaReferencer.cs
@@ -39,7 +39,6 @@ namespace Cpln.Enigmos
             enigmas.Add(new Enigma(new NbrCarresEnigmaPanel(), "Nombre de carrés"));
             enigmas.Add(new Enigma(new AppuieReponseEnigmaPanel(), "Appuie sur la réponse"));
             enigmas.Add(new Enigma(new CaseVideEnigmaPanel(), "Et ben non"));
-            enigmas.Add(new Enigma(new PenduEnigmaPanel(), "Le jeu du pendu"));
             enigmas.Add(new Enigma(new HazardEnigmaPanel(), "Hazard"));
             enigmas.Add(new Enigma(new MemoireDesChiffresEnigmaPanel(), "Souviens toi"));
             enigmas.Add(new Enigma(new DingbatEnigmaPanel(), "Jeu de mot"));
