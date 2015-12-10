8d997b3781dbb7c3d94e396d507901c0c6aa77ee
Gümpel Rémy <GuempelR@s2.rpn.ch>
2015-12-10 11:40:22 +0100

    Ajout d'images, continuation du code.

--- Enigmas/BeatThemAllEnigmaPanel.cs
+++ Enigmas/BeatThemAllEnigmaPanel.cs
@@ -8,12 +8,11 @@ using System.Windows.Forms;
 
 namespace Cpln.Enigmos.Enigmas
 {
-
-    /// <summary>
-    /// 
-    /// </summary>
     public class BeatThemAllEnigmaPanel : EnigmaPanel
     {
+        //constante permettant de gérer les caractéristique rapidement
+        const int WIDTH_PUNCH = 70, CARACTER_SPEED = 20;
+
         private Timer Timer = new Timer();
 
         private PictureBox pbxPlayer = new PictureBox();
@@ -21,9 +20,9 @@ namespace Cpln.Enigmos.Enigmas
 
         List<PictureBox> listEnnemi;
 
-        bool bIsRight = true, bSpawnEnnemiRight = true;
+        bool bIsLeft = true, bSpawnEnnemiRight = true, bPunch = false, bGodMod = false;
 
-        int iTimer, iNbrEnnemi;
+        int iTimer = 0, iNbrEnnemi = 0, iTimerPunch = 0;
 
         /// <summary>
         /// Constructeur par défaut, génère les picturesBox de base (le sol, le personnage du joueur).
@@ -34,20 +33,21 @@ namespace Cpln.Enigmos.Enigmas
             Screen myScreen = Screen.PrimaryScreen;
             this.Width = (myScreen.WorkingArea.Width);
             this.Height = (myScreen.WorkingArea.Height)/2;
+            this.BackColor = Color.Black;
 
             //Mise en place du sol
             pbxGround.Size = new Size(this.Width, this.Height/7);
             pbxGround.Location = new Point(this.Width / 2 - pbxGround.Width / 2, this.Bottom - pbxGround.Height);
-            pbxGround.BackColor = Color.Red;
+            pbxGround.BackColor = Color.White;
             Controls.Add(pbxGround);
 
             //Mise en place du personnage joueur
-            pbxPlayer.Size = new Size(80, 100);
+            pbxPlayer.Size = Properties.Resources.pj1.Size; ;
             pbxPlayer.Location = new Point(pbxGround.Width / 2 - pbxPlayer.Width / 2, pbxGround.Top - pbxPlayer.Height);
-            pbxPlayer.BackColor = Color.Green;
+            pbxPlayer.BackColor = Color.Transparent;
             Controls.Add(pbxPlayer);
  
-           //Mise en place d'un timer
+           //Mise en place d'un timer (gérants les déplacements ennemie)
             Timer.Interval = 1; // 1 milliseconde
             Timer.Tick += new EventHandler(Timer_Tick);
             Timer.Start();
@@ -58,25 +58,33 @@ namespace Cpln.Enigmos.Enigmas
             this.Focus();
         }
 
-        //Fonction permettant de gèrer les déplacments 
+        //Fonction permettant de gèrer les input du joueur (déplacement, punch, saut)
         public override void PressKey(object sender, KeyEventArgs e) 
         {
+            //droite
             if (e.KeyCode == Keys.D)
             {
                 if (pbxPlayer.Left >= this.Left)
                 {
-                    bIsRight = false;
-                    pbxPlayer.Left += 10;
+                    bIsLeft = false;
+                    pbxPlayer.Left += CARACTER_SPEED;
                 }
             }
+            //gauche
             if (e.KeyCode == Keys.A)
             {
                 if (pbxPlayer.Right <= this.Right)
                 {
-                    bIsRight = true;
-                    pbxPlayer.Left -= 10;
+                    bIsLeft = true;
+                    pbxPlayer.Left -= CARACTER_SPEED;
                 }
             }
+            //saut
+            if (e.KeyCode == Keys.W)
+            {
+                
+            }
+            //punch
             if (e.KeyCode == Keys.Space)
             {
                 Punch();
@@ -87,31 +95,41 @@ namespace Cpln.Enigmos.Enigmas
         private void Punch()
         {
             //punch a droit
-            if(bIsRight==true)
+            if(bIsLeft==true)
             {
-                pbxPlayer.Left -= 90;
-                pbxPlayer.Width += 90;
-                //changer image
+                pbxPlayer.Left -= WIDTH_PUNCH;
+                pbxPlayer.Width += WIDTH_PUNCH;
             }
             //punch a gauche
             else
             {
-                pbxPlayer.Width += 90;
-                //changer image
+                pbxPlayer.Width += WIDTH_PUNCH;
             }
+            bPunch = true;
         }
 
         //Timer gérant les spawns 
         private void Timer_Tick(object sender, EventArgs e)
         {
             iTimer++;
-            if (iTimer == 140)
+
+            //Gére les images
+            if(bIsLeft==true)
+            {
+                pbxPlayer.Image = Properties.Resources.pj3;
+            }
+            else
+            {
+                pbxPlayer.Image = Properties.Resources.pj1;
+            }
+            //Crée les ennemies
+            if (iTimer == 40)
             {
                 //inverse le point de spawn
                 bSpawnEnnemiRight = !bSpawnEnnemiRight;
 
                 PictureBox pbxEnnemi = new PictureBox();
-                pbxEnnemi.Size = new Size(60, 60);
+                pbxEnnemi.Size = Properties.Resources.ennemi1.Size;
                 if (bSpawnEnnemiRight == true)
                 {
                     pbxEnnemi.Location = new Point(pbxGround.Left - pbxEnnemi.Width, pbxGround.Top - pbxEnnemi.Height);
@@ -120,21 +138,63 @@ namespace Cpln.Enigmos.Enigmas
                 {
                     pbxEnnemi.Location = new Point(pbxGround.Right, pbxGround.Top - pbxEnnemi.Height);
                 }
-                pbxEnnemi.BackColor = Color.DarkRed;
+                pbxEnnemi.BackColor = Color.Transparent;
+                pbxEnnemi.Image = Properties.Resources.ennemi1;
                 this.Controls.Add(pbxEnnemi);
 
                 listEnnemi.Add(pbxEnnemi);
                 iTimer = 0;
             }
+
+            //fais avancer les ennemies
             for (int i = 0; i < listEnnemi.Count; i++)
             {
                 if (i % 2 == 0)
                 {
-                    listEnnemi[i].Left -= 2;
+                    listEnnemi[i].Left -= 6;
                 }
                 else
                 {
-                    listEnnemi[i].Left += 2;
+                    listEnnemi[i].Left += 6;
+                }
+
+                //HitBox des ennemies
+                if (listEnnemi[i].Right >= pbxPlayer.Left && listEnnemi[i].Top <= pbxPlayer.Bottom  && listEnnemi[i].Left <= pbxPlayer.Right)
+                {
+                    if (bGodMod == true)
+                    {
+                        Controls.Remove(listEnnemi[i]);
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+
+            //Gére les coups de poingt
+            if(bPunch==true)
+            {
+                bGodMod = true;
+                iTimerPunch++;
+                if (bIsLeft)
+                {
+                    pbxPlayer.Image = Properties.Resources.pj4;
+                }
+                else
+                {
+                    pbxPlayer.Image = Properties.Resources.pj2;
+                }
+
+                if (iTimerPunch == 15)
+                {
+                    if (bIsLeft)
+                    {
+                        pbxPlayer.Left += WIDTH_PUNCH;
+                    }
+                    pbxPlayer.Width  = Properties.Resources.pj1.Width;
+                    bGodMod = false;
+                    iTimerPunch = 0;
+                    bPunch = false;
                 }
             }
         }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -138,6 +138,16 @@ namespace Cpln.Enigmos.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ennemi1 {
+            get {
+                object obj = ResourceManager.GetObject("ennemi1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap OpenDoor {
             get {
                 object obj = ResourceManager.GetObject("OpenDoor", resourceCulture);
@@ -181,6 +191,46 @@ namespace Cpln.Enigmos.Properties {
         internal static System.Drawing.Bitmap PartiJuste {
             get {
                 object obj = ResourceManager.GetObject("PartiJuste", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pj1 {
+            get {
+                object obj = ResourceManager.GetObject("pj1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pj2 {
+            get {
+                object obj = ResourceManager.GetObject("pj2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pj3 {
+            get {
+                object obj = ResourceManager.GetObject("pj3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pj4 {
+            get {
+                object obj = ResourceManager.GetObject("pj4", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
