1315e88704d07e173f20877e8f722921034317a8
Bécaud Arthur <BecaudAr@s2.rpn.ch>
2016-01-06 16:22:45 +0100

    Code pour afficher traits avec PictureBox

--- Enigmas/NinePointsEnigmaPanel.cs
+++ Enigmas/NinePointsEnigmaPanel.cs
@@ -14,85 +14,68 @@ namespace Cpln.Enigmos.Enigmas
         private Jeton[] tJeton = new Jeton[9];
         private CaseVide[] tCaseVide = new CaseVide[16];
         private TableLayoutPanel TlpCase9Points;
-        private int[,] tSaveMouseClickPosition = new int[5, 2];
+        private PictureBox pcbCase9Points = new PictureBox();
+        private Point[] tSaveMouseClickPosition = new Point[5];
 
         /// <summary>
         /// Charge l'énigme des 9 points.
         /// </summary>
         public NinePointsEnigmaPanel()
         {
-            Affiche9Points();
+            Affiche9Point();
 
         }
 
-        /// <summary>
-        /// Créer la structure de l'énigme et affiche les 9 points
-        /// </summary>
-        private void Affiche9Points()
+        public void Affiche9Point()
         {
             this.Height = 800;
-            this.BackColor = Color.Black;
-
-            /*Créer tableau 5x5*/
-            TlpCase9Points = new TableLayoutPanel();
-            TlpCase9Points.ColumnCount = 5;
-            for (int i = 0; i < 5; i++)
-                TlpCase9Points.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 0.2f));
-            TlpCase9Points.RowCount = 5;
-            for (int i = 0; i < 5; i++)
-                TlpCase9Points.RowStyles.Add(new RowStyle(SizeType.Percent, 0.2f));
-            TlpCase9Points.Dock = DockStyle.Fill;
-
-            Controls.Add(TlpCase9Points);
-
-            /*Affiche les 9 points de l'énigme*/
-            for (int iY = 1, i = 0; iY <= 3; iY++)
-                for (int iX = 1; iX <= 3; iX++, i++)
-                    tJeton[i] = new Jeton(iX, iY, this, TlpCase9Points);
-
-            /*Créer les cases autour des 9 points*/
-            for (int iY = 0, i = 0; iY < 5; iY+= 4)
-                for (int iX = 0; iX < 5; iX++, i++)
-                    tCaseVide[i] = new CaseVide(iX, iY, this, TlpCase9Points);
-
-            for (int iX = 0, i = 10; iX < 5; iX += 4)
-                for (int iY = 1; iY < 4; iY++, i++)
-                    tCaseVide[i] = new CaseVide(iX, iY, this, TlpCase9Points);
+            pcbCase9Points.Size = this.Size;
+            pcbCase9Points.Click += new System.EventHandler(MouseClick_Affiche9Points);
+            pcbCase9Points.Paint += new PaintEventHandler(Paint_pcbCase9Points);
+            Controls.Add(pcbCase9Points);
         }
 
-        private void AfficheTrait(object sender, int iX, int iY)
+        private void Paint_pcbCase9Points(object sender, PaintEventArgs e)
         {
-            int iChoix = 0, iFirstCaseEmpty = -1, iDirectionX, iDirectionY;
-            
-            /*Regarde la position(0 à 4) de la dernière position sauvegardé*/
+            Pen pen = new Pen(Color.Black, 10);
+
+            for (int i = 1; i < tSaveMouseClickPosition.Length; i++)
+            {
+                if (tSaveMouseClickPosition[i].IsEmpty)
+                {
+                    break;
+                }
+                e.Graphics.DrawLine(pen, tSaveMouseClickPosition[i - 1].X - this.Left, tSaveMouseClickPosition[i - 1].Y - this.Top, tSaveMouseClickPosition[i].X - this.Left, tSaveMouseClickPosition[i].Y - this.Top);
+            }
+        }
+
+        private void MouseClick_Affiche9Points(object sender, EventArgs e)
+        {
+            int iFirstCaseEmpty = 0;
+
+            /*Trouve la première case vide*/
             for (int i = 0; i < tSaveMouseClickPosition.Length; i++)
-                if (tSaveMouseClickPosition[i, 0] >= 0)
+            {
+                if (tSaveMouseClickPosition[i].IsEmpty)
                 {
                     iFirstCaseEmpty = i;
-                    i += 10;
+                    break;
                 }
+            }
 
             /*Ajoute les dernières positions connues*/
-            tSaveMouseClickPosition[iFirstCaseEmpty, 0] = iX;
-            tSaveMouseClickPosition[iFirstCaseEmpty, 1] = iY;
+            tSaveMouseClickPosition[iFirstCaseEmpty] = Cursor.Position;
 
-            /*Définit la direction dans laquel le trait va aller dans au axe x, y*/
-            if (tSaveMouseClickPosition[iFirstCaseEmpty - 1, 0] > tSaveMouseClickPosition[iFirstCaseEmpty, 0])
-                iDirectionX = -1;
-            else if (tSaveMouseClickPosition[iFirstCaseEmpty - 1, 0] == tSaveMouseClickPosition[iFirstCaseEmpty, 0])
-                iDirectionX = 0;
-            else if (tSaveMouseClickPosition[iFirstCaseEmpty - 1, 0] < tSaveMouseClickPosition[iFirstCaseEmpty, 0])
-                iDirectionX = 1;
+            /*Éxecute la suite du code si le numéro de la première case vide est impaire*/
+            if (iFirstCaseEmpty != 0)
+            {
+                pcbCase9Points.Invalidate();
+            }
 
-            if (tSaveMouseClickPosition[iFirstCaseEmpty - 1, 1] > tSaveMouseClickPosition[iFirstCaseEmpty, 1])
-                iDirectionY = -1;
-            else if (tSaveMouseClickPosition[iFirstCaseEmpty - 1, 1] == tSaveMouseClickPosition[iFirstCaseEmpty, 1])
-                iDirectionY = 0;
-            else if (tSaveMouseClickPosition[iFirstCaseEmpty - 1, 1] < tSaveMouseClickPosition[iFirstCaseEmpty, 1])
-                iDirectionY = 1;
-
-                
-            
+            if(iFirstCaseEmpty == 4)
+            {
+                //tSaveMouseClickPosition = new int[5, 2];
+            }
         }
     }
 }
