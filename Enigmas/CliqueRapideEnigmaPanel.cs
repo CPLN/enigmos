c76f1a5f79c61c69717c8038a5d354281b6368e4
Loison Yohann <LoisonYo@s2.rpn.ch>
2016-01-14 09:49:55 +0100

    Ajout du bouton à cliquer

--- Enigmas/CliqueRapideEnigmaPanel.cs
+++ Enigmas/CliqueRapideEnigmaPanel.cs
@@ -1,13 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Cpln.Enigmos.Enigmas
 {
     class CliqueRapideEnigmaPanel : EnigmaPanel
     {
+        private Button btnACliquer = new Button();
         
+        public CliqueRapideEnigmaPanel()
+        {
+            btnACliquer.Size = new Size(200, 200);
+            btnACliquer.Location = new Point(400 - btnACliquer.Width / 2, 300 - btnACliquer.Height / 2);
+            //ChangementPourRouge();
+            Controls.Add(btnACliquer);
+        }
     }
 }
