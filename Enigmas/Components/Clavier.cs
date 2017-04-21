9c969c426ab1306ee4da89661c33ee1b1de1a349
Rime Vincent <RimeVi@s2.rpn.ch>
2017-04-21 11:26:19 +0200

    Création de la classe clavier

---
+++ Enigmas/Components/Clavier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cpln.Enigmos.Enigmas.Components
+{
+    class Clavier
+    {
+        //List<Panel> LesTouches = new List<Panel>();
+        //Panel pnlCtrl, pnlWindows, pnlAlt, pnlSpace, pnlAltgr, pnlMaj, pnlDash, pnlPoint, pnlComa, pnlCapslock, pnlTab, pnlEnter, pnlDelete, pnlApostrophie, pnl1, pnl2, pnl3, pnl4, pnl5, pnl6, pnl7, pnl8, pnl9, pnl0, pnlQ, pnlW, pnlE, pnlR, pnlT, pnlZ, pnlU, pnlI, pnlO, pnlP, pnlA, pnlS, pnlD, pnlF, pnlG, pnlH, pnlJ, pnlK, pnlL, pnlY, pnlX, pnlC, pnlV, pnlB, pnlN, pnlM;
+        //public List<Panel> LesTouches1
+        //{
+        //    get
+        //    {
+        //        return LesTouches;
+        //    }
+
+        //    set
+        //    {
+        //        LesTouches1.All(pnlCtrl, pnlWindows, pnlAlt, pnlSpace, pnlAltgr, pnlMaj, pnlDash, pnlPoint, pnlComa, pnlCapslock, pnlTab, pnlEnter, pnlDelete, pnlApostrophie, pnl1, pnl2, pnl3, pnl4, pnl5, pnl6, pnl7, pnl8, pnl9, pnl0, pnlQ, pnlW, pnlE, pnlR, pnlT, pnlZ, pnlU, pnlI, pnlO, pnlP, pnlA, pnlS, pnlD, pnlF, pnlG, pnlH, pnlJ, pnlK, pnlL, pnlY, pnlX, pnlC, pnlV, pnlB, pnlN, pnlM);
+        //        LesTouches = value;
+        //    }
+        //Attribut
+        private List<Panel> pnlTouches = new List<Panel>();
+        private double dblWidthPanel;
+        private double dbliHeightPanel;
+
+        //Constructeur
+
+
+
+    }
+}
+}
