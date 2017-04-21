f421b5d9b1ba004ae9104bb067aa9f8eaaab4726
AlanMortellaro <alan.mortellaro@hotmail.com>
2017-04-21 11:46:58 +0200

    Instancier un panel

--- Enigmas/CodeBarreEnigmaPanel.cs
+++ Enigmas/CodeBarreEnigmaPanel.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using Cpln.Enigmos.Enigmas.Components;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Cpln.Enigmos.Enigmas
 {
     class CodeBarreEnigmaPanel : EnigmaPanel
     {
-
+        Panel pBarreE = new Panel();
+        public CodeBarreEnigmaPanel()
+        {
+            pBarreE.Left = 121;
+            pBarreE.Top = 11;
+            pBarreE.Size = new Size(10, 200);
+            pBarreE.Location = new Point(100, 200);
+            pBarreE.BackColor = Color.Black;
+            this.Controls.Add(pBarreE);
+        }
     }
 }
