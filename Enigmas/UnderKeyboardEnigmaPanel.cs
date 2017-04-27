5d84b3ae3798e08442522c9c779b03f41dd71c9f
Rime <vincent.rime@rpn.ch>
2017-04-27 16:21:03 +0200

    bouclhe for pour les touches

--- Enigmas/UnderKeyboardEnigmaPanel.cs
+++ Enigmas/UnderKeyboardEnigmaPanel.cs
@@ -11,8 +11,8 @@ namespace Cpln.Enigmos.Enigmas
     class UnderKeyboardEnigmaPanel : EnigmaPanel
     {
         private List<Touche> touches;
-        private int iWidth = 20;
-        private int iHeight = 20;
+        private int iWidth = 60;
+        private int iHeight = 60;
         private int iLocalisationX = 50;
         private int iLocalisationY = 50;
 
@@ -21,16 +21,20 @@ namespace Cpln.Enigmos.Enigmas
             touches = new List<Touche>();
             int iLocX = iLocalisationX;
             int iLocY = iLocalisationY;
-            for (int i = 0; i < 61; i++)
+            for (int i = 0; i < 26; i++)
             {
-
-
                 iLocX += iWidth;
-                if (i%13 == 0)
+                if (i%7 == 0)
                 {
                     iLocX = iLocalisationX;
-                    iLocY = iLocalisationY;
-                    iLocalisationY = iLocY;
+                    iLocY += iLocalisationY + iHeight;
+                   
+                }
+                if (i == 21)
+                {
+                    iLocX = iLocalisationX;
+                    iLocY += iLocalisationY + Height * 2;
+
                 }
                 Touche touche = new Touche("", iLocX, iLocY,iWidth,iHeight);
                 touche.BackColor = Color.Black;
