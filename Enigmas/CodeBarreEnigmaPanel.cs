8d59374ee93da39f56bbda3d41eba41c4372b232
AlanMortellaro <alan.mortellaro@hotmail.com>
2017-04-27 16:13:20 +0200

    Ajout de la méthode DeplacerBarre

--- Enigmas/CodeBarreEnigmaPanel.cs
+++ Enigmas/CodeBarreEnigmaPanel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,14 @@ namespace Cpln.Enigmos.Enigmas
     class CodeBarreEnigmaPanel : EnigmaPanel
     {
         Panel pBarreE = new Panel();
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
         public CodeBarreEnigmaPanel()
         {
             pBarreE.Left = 121;
@@ -19,24 +28,15 @@ namespace Cpln.Enigmos.Enigmas
             pBarreE.Size = new Size(10, 200);
             pBarreE.Location = new Point(100, 200);
             pBarreE.BackColor = Color.Black;
+            pBarreE.MouseMove += new MouseEventHandler(DeplacerBarre);
             this.Controls.Add(pBarreE);
         }
-        Point positionClick; // Suffisamment clair, un point c'est tout !
-        protected override void OnMouseDown(MouseEventArgs e) // Surcharge de la méthode OnMouseDown
+        private void DeplacerBarre(object sender, MouseEventArgs e)
         {
-            positionClick = e.Location; // Où on initialise le point
-            base.OnMouseDown(e); // J'imagine que cette ligne va intégrer le reste de la méthode mère
-            // (comme une concaténation), mais je suis pas sûr...
-        }
-
-        protected override void OnMouseMove(MouseEventArgs e) // Surcharge de la méthode OnMouseMove
-        {
-            if (e.Button == MouseButtons.Left) // Si on clique (Gauche)
+            if (e.Button == MouseButtons.Left)
             {
-                pBarreE.Location = new Point(Location.X + e.X - positionClick.X, Location.Y + e.Y - positionClick.Y);
-                // Ça change l'origine du la DamiBox(j'adore)...
-                // Locution = Point d'origine de la DamiBox
-                // e.X/e.Y = int de l'abscisse/ordonnée de la souris
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
             }
         }
     }
