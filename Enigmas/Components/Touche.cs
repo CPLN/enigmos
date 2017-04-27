cf83b9c1f3be54e5697b7ff8c872c4fc712df0bf
Rime <vincent.rime@rpn.ch>
2017-04-27 14:02:19 +0200

    Création de liste pour les touches

--- Enigmas/Components/Touche.cs
+++ Enigmas/Components/Touche.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Cpln.Enigmos.Enigmas.Components
 {
     class Touche : Panel
--- Enigmas/UnderKeyboardEnigmaPanel.cs
+++ Enigmas/UnderKeyboardEnigmaPanel.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cpln.Enigmos.Enigmas.Components;
 
 namespace Cpln.Enigmos.Enigmas
 {
     class UnderKeyboardEnigmaPanel : EnigmaPanel
     {
-        
+        private List<Touche> LesTouches;
+
+        public UnderKeyboardEnigmaPanel()
+        {
+            LesTouches = new List<Touche>();
+            LesTouches.Add(new Touche("", 0, 0));
+        }
     }
 }
