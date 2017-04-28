f3f790551fbaad145a2c3514035a4ff51edd1054
Rime <vincent.rime@rpn.ch>
2017-04-28 12:12:28 +0200

    la magie des touche qui bouge

--- Enigmas/Components/Touche.cs
+++ Enigmas/Components/Touche.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 
 namespace Cpln.Enigmos.Enigmas.Components
 {
-    class Touche : Panel
+    class Touche : MovablePanel
     {
         //List<Panel> LesTouches = new List<Panel>();
         //Panel pnlCtrl, pnlWindows, pnlAlt, pnlSpace, pnlAltgr, pnlMaj, pnlDash, pnlPoint, pnlComa, pnlCapslock, pnlTab, pnlEnter, pnlDelete, pnlApostrophie, pnl1, pnl2, pnl3, pnl4, pnl5, pnl6, pnl7, pnl8, pnl9, pnl0, pnlQ, pnlW, pnlE, pnlR, pnlT, pnlZ, pnlU, pnlI, pnlO, pnlP, pnlA, pnlS, pnlD, pnlF, pnlG, pnlH, pnlJ, pnlK, pnlL, pnlY, pnlX, pnlC, pnlV, pnlB, pnlN, pnlM;
