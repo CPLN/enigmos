f6f2d3d34b03d2485bf463303943bc778efbdcf5
misterkonik <durelnicolas1@gmail.com>
2017-05-05 10:43:39 +0200

    Je fais l'événemet presskey pour mettre les panels en invisble si la bonne touche est pressé

--- Enigmas/CoucheCouleurEnigmaPanel.cs
+++ Enigmas/CoucheCouleurEnigmaPanel.cs
@@ -11,28 +11,43 @@ namespace Cpln.Enigmos.Enigmas
    public class CoucheCouleurEnigmaPanel : EnigmaPanel
     {
         private List<Panel> PanelCouche;
+        private string[] tabCouleur;
+        private char[] tabReponse;
+        private int iCpt;
+        private Label lblReponse;
         /// <summary>
         /// Constructeur pas défaut
         /// </summary>
         public CoucheCouleurEnigmaPanel()
         {
-            string[] tabCouleur = new string[7] { "Orange", "Grey", "Blue", "Green", "Pink", "Red", "White" };
-
+            iCpt = 0;
+            tabCouleur = new string[7] { "Purple", "Orange", "Blue", "Green", "Pink", "Red", "Yellow" };
+            tabReponse = new char[7] { 'T', 'R', 'O', 'L', 'L', 'E', 'R' };
             PanelCouche = new List<Panel>();
             for (int i = 0;i<tabCouleur.Length; i++)
             {
                 Panel panel = new Panel();
-                panel.Location = new System.Drawing.Point(100, 100);
-                panel.Size = new System.Drawing.Size(50, 50);
+                panel.Location = new System.Drawing.Point(0, 0);
+                panel.Size = new System.Drawing.Size(800, 600);
                 panel.BackColor = System.Drawing.Color.FromName(tabCouleur[i]);
                 PanelCouche.Add(panel);
-            }       
-             PanelCouche.ForEach(this.Controls.Add);   
+            }     
+            PanelCouche.ForEach(this.Controls.Add);
+
+            lblReponse = new Label();
+            lblReponse.Location = new Point(100, 100);
+            lblReponse.Text = "La réponse est la suite de caractère que vous venez d'entrer";
+            Controls.Add(lblReponse);
         }
 
         public override void PressKey(object sender, KeyEventArgs e)
         {
-            
+                if(Convert.ToChar(e.KeyValue) == tabReponse[iCpt])
+                {
+                    PanelCouche[iCpt].Visible = false;
+                if(iCpt<6)
+                    iCpt++;
+                } 
         }
     }
 }
