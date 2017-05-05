eac985b68b9672ac5a74dcee061d66bd88708742
misterkonik <durelnicolas1@gmail.com>
2017-05-05 12:08:22 +0200

    Ajout d'une image et d'un bouton reste et du label d'indice de réponse

--- Enigmas/CoucheCouleurEnigmaPanel.cs
+++ Enigmas/CoucheCouleurEnigmaPanel.cs
@@ -14,16 +14,26 @@ namespace Cpln.Enigmos.Enigmas
         private string[] tabCouleur;
         private char[] tabReponse;
         private int iCpt;
+        private const int iLongeurReponse = 7;
         private Label lblReponse;
+        private PictureBox ptbTrolle;
+        private Label lblReset;
         /// <summary>
         /// Constructeur pas défaut
         /// </summary>
         public CoucheCouleurEnigmaPanel()
         {
             iCpt = 0;
-            tabCouleur = new string[7] { "Purple", "Orange", "Blue", "Green", "Pink", "Red", "Yellow" };
-            tabReponse = new char[7] { 'T', 'R', 'O', 'L', 'L', 'E', 'R' };
+            tabCouleur = new string[iLongeurReponse] { "Purple", "Orange", "Blue", "Green", "Pink", "Red", "Yellow" };
+            tabReponse = new char[iLongeurReponse] { 'T', 'R', 'O', 'L', 'L', 'E', 'R' };
             PanelCouche = new List<Panel>();
+
+            lblReset = new Label();
+            lblReset.Text = "Recommencer";
+            lblReset.Click += new EventHandler(Reset);
+            Controls.Add(lblReset);
+
+            //création de tous les panels en fonction de la longueur de la Réponse avec juste la couleur qui change
             for (int i = 0;i<tabCouleur.Length; i++)
             {
                 Panel panel = new Panel();
@@ -35,13 +45,21 @@ namespace Cpln.Enigmos.Enigmas
             PanelCouche.ForEach(this.Controls.Add);
 
             lblReponse = new Label();
-            lblReponse.Location = new Point(100, 100);
-            lblReponse.Text = "La réponse est la suite de caractère que vous venez d'entrer";
+            lblReponse.Location = new Point(100, 50);
+            lblReponse.Text = "La réponse est la suite de caractère que vous venez d'entrer.";
+            lblReponse.Width = 300;
             Controls.Add(lblReponse);
+
+            ptbTrolle = new PictureBox();
+            ptbTrolle.Location = new Point(200, 100);
+            ptbTrolle.Size = new Size(1000, 1000);
+            ptbTrolle.Image = Properties.Resources.TrollFace;
+            Controls.Add(ptbTrolle);
         }
 
         public override void PressKey(object sender, KeyEventArgs e)
         {
+            //regarde si le caractère entré est égale à la case du tableau
                 if(Convert.ToChar(e.KeyValue) == tabReponse[iCpt])
                 {
                     PanelCouche[iCpt].Visible = false;
@@ -49,5 +67,14 @@ namespace Cpln.Enigmos.Enigmas
                     iCpt++;
                 } 
         }
+        private void Reset(object sender, EventArgs e)
+        {
+            iCpt = 0;
+            foreach(Panel panel in PanelCouche)
+            {
+                panel.Visible = true;
+            }
+        }
     }
+
 }
