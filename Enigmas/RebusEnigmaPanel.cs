0ae8452354e84758c35ddc9ac5bc7e9cf0960557
danydacosta <danydc@comac.ch>
2017-05-08 10:53:09 +0200

    Création de la class pour le jeu du rébus, ajout des infomrations dans le fichier xml

---
+++ Enigmas/RebusEnigmaPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cpln.Enigmos.Enigmas
+{
+    /// <summary>
+    /// Class de l'énigme du rébus
+    /// </summary>
+    public class RebusEnigmaPanel : EnigmaPanel
+    {
+        public RebusEnigmaPanel()
+        {
+            //Lors de l'instantation, peupler une liste d'image contenant les 5 images possibles.
+            //Piocher dans cette liste une image aléatoire qui sera l'image affichée.
+        }
+    }
+}
