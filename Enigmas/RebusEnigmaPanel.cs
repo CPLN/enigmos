07ed1b0312e2b637672175a61e7f0444c016645e
danydacosta <danydc@comac.ch>
2017-05-08 11:47:45 +0200

    Affichage des éléments sur la page (titre, image, textbox, boutton)

--- Enigmas/RebusEnigmaPanel.cs
+++ Enigmas/RebusEnigmaPanel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Cpln.Enigmos.Enigmas
 {
@@ -11,10 +13,72 @@ namespace Cpln.Enigmos.Enigmas
     /// </summary>
     public class RebusEnigmaPanel : EnigmaPanel
     {
+        #region Attributs & Propriétés
+        List<PictureBox> lImg = new List<PictureBox>();
+        Label lblTitle = new Label { Text = "Rébus" };
+        TextBox tbxAnswer = new TextBox { Text = "Réponse", Width=200 };
+        TableLayoutPanel display = new TableLayoutPanel();
+        Button bntDone = new Button { Text = "Valider" };
+        #endregion
+
+        #region Contructeur class & page
         public RebusEnigmaPanel()
         {
             //Lors de l'instantation, peupler une liste d'image contenant les 5 images possibles.
             //Piocher dans cette liste une image aléatoire qui sera l'image affichée.
+            lImg.Add(new PictureBox { BackgroundImage = Properties.Resources.Coccinelle, Size = new Size(550, 240) });
+            lImg.Add(new PictureBox { BackgroundImage = Properties.Resources.farine, Size = new Size(500, 500) });
+            lImg.Add(new PictureBox { BackgroundImage = Properties.Resources.mamifere, Size = new Size(512, 261) });
+            lImg.Add(new PictureBox { BackgroundImage = Properties.Resources.parapluie, Size = new Size(550, 240) });
+            lImg.Add(new PictureBox { BackgroundImage = Properties.Resources.piano, Size = new Size(550, 240) });
+
+            //Définit la source de l'affichage
+            display.Dock = DockStyle.Fill;
+            Controls.Add(display);
+
+            //Construit l'affichage
+            BuildPage();
         }
+
+        public void BuildPage()
+        {
+            //Ajoute les évènements sur la texbox
+            tbxAnswer.GotFocus += RemoveText;
+
+            // Suppression de la mise en page des réponses
+            display.Controls.Clear();
+
+            // Création de la mise en page de la question
+            display.ColumnCount = 5;
+            display.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 0.5f));
+            display.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+            display.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+            display.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+            display.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 0.5f));
+            display.RowCount = 4;
+            display.RowStyles.Add(new RowStyle(SizeType.Percent, 0.5f));
+            display.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            display.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            display.RowStyles.Add(new RowStyle(SizeType.Percent, 0.5f));
+
+            // Mise en forme du label de titre
+            lblTitle.Font = new Font(FontFamily.GenericSansSerif, 24, FontStyle.Bold);
+            lblTitle.AutoSize = true;
+            display.SetColumnSpan(lblTitle, 3);
+
+            // Coordonnées des objets
+            display.Controls.Add(lblTitle, 1, 0);
+            display.Controls.Add(lImg[2], 1, 1);
+            display.Controls.Add(tbxAnswer, 1, 2);
+            display.Controls.Add(bntDone, 1, 3);
+        }
+        #endregion
+
+        #region Evènements
+        public void RemoveText(object sender, EventArgs e)
+        {
+            tbxAnswer.Text = "";
+        }
+        #endregion
     }
 }
