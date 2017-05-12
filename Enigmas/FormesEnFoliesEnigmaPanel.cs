fc50905b536b770ffae94a5055c1c17913ab7ac8
Sandozor <robin.sandoz-othenin@rpn.ch>
2017-05-12 12:17:03 +0200

    Création méthode ajoutdansliste

--- Enigmas/FormesEnFoliesEnigmaPanel.cs
+++ Enigmas/FormesEnFoliesEnigmaPanel.cs
@@ -12,6 +12,8 @@ namespace Cpln.Enigmos.Enigmas
 {
     public class FormesEnFoliesEnigmaPanel : EnigmaPanel 
     {
+        int Compteur = 0;
+
         List<Panel> lstForme = new List<Panel>(); // Création d'une liste qui contiendra tous les panels (formes)
 
         Button btnDifferent = new Button(); // Création du bouton différent
@@ -23,19 +25,18 @@ namespace Cpln.Enigmos.Enigmas
         public FormesEnFoliesEnigmaPanel()
         {
             Triangle triangle = new Triangle(100, 50); // Création du panel qui sera un triangle, avec une base de 100 et un sommet à 50 (en haut milieu du panel)
-            Controls.Add(triangle);
+            AjoutDansListe(triangle);
 
             Ellipse ellipse = new Ellipse();
             ellipse.Location = new Point(250, 250);
             ellipse.Size = new Size(200, 200);
-            Controls.Add(ellipse);
-            lstForme.Add(ellipse);
+            AjoutDansListe(ellipse);
 
             Controls.Add(btnDifferent);
             btnDifferent.Location = new Point(421, 111);
             btnDifferent.Text = "Différent";
             btnDifferent.Click += new EventHandler(ClicSurBouton);
-
+            
             Controls.Add(btnIdentique);
             btnIdentique.Location = new Point(321, 111);
             btnIdentique.Text = "Identique";
@@ -43,21 +44,32 @@ namespace Cpln.Enigmos.Enigmas
 
             Carre1 = AjoutPanelCarre(50, 250, 250, Carre1);
             Carre1.BackColor = Color.Blue;
+            AjoutDansListe(Carre1);
 
             Carre2 = AjoutPanelCarre(50, 350, 350, Carre2);
             Carre2.BackColor = Color.Black;
+            AjoutDansListe(Carre2);
         }
 
         private void ClicSurBouton(object sender, EventArgs e)
         {
-            
+
+            if (btnDifferent == sender)
+            {
+                Controls.Add(lstForme[Compteur]);
+            }
+            else
+            {
+                Controls.Add(lstForme[Compteur]);
+            }
+
+            Compteur++;
         }
 
         public Panel AjoutPanelCarre(int cote, int positionX, int positionY, Panel p)
         {
             p.Size = new Size(cote, cote);
             p.Location = new Point(positionX, positionY);
-            lstForme.Add(p);
             
             return p;
         }
@@ -66,11 +78,16 @@ namespace Cpln.Enigmos.Enigmas
         {
             p.Size = new Size(largeur, hauteur);
             p.Location = new Point(positionX, positionY);
-            lstForme.Add(p);
 
             return p;
         }
 
+        public Panel AjoutDansListe(Panel p)
+        {
+            lstForme.Add(p);
+
+            return p;
+        }
 
     }
 }
