c9e3207a923c43d0fd4a91fc754de69ef13ccc12
Steeve Droz <steeve.droz@rpn.ch>
2017-05-15 11:33:36 +0200

    Ajout de l'énigme Switch

---
+++ Enigmas/SwitchesEnigmaPanel.cs
@@ -0,0 +1,12 @@
+﻿using Cpln.Enigmos.Enigmas;
+
+namespace Cpln.Enigmos.Enigmas
+{
+    /// <summary>
+    /// Cette énigme demande au joueur d'allumer toutes les cases, sachant qu'un clic sur une case bascule la couleur de toutes les cases adjascentes.
+    /// </summary>
+    class SwitchesEnigmaPanel : EnigmaPanel
+    {
+        private List<Panel> panels;
+    }
+}
