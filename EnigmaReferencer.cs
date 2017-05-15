9139dbc4b944d4e723f798cf48f9cd1889b3eb07
OwenGombas <owen.gombas@gmail.com>
2017-05-15 11:49:09 +0200

    Améliorations

--- EnigmaReferencer.cs
+++ EnigmaReferencer.cs
@@ -28,38 +28,36 @@ namespace Cpln.Enigmos
         {
             ShuffleList<Enigma> enigmas = new ShuffleList<Enigma>();
 
-<<<<<<< HEAD
-            //enigmas.Add(new Enigma(new AppuieReponseEnigmaPanel(), "Appuie sur la réponse"));
-            //enigmas.Add(new Enigma(new BeatThemAllEnigmaPanel(), "Beat them all"));
-            //enigmas.Add(new Enigma(new BusEnigmaPanel(), "Sens du bus"));
-            //enigmas.Add(new Enigma(new CaseVideEnigmaPanel(), "Et ben non"));
-            //enigmas.Add(new Enigma(new CharadeEnigmaPanel(), "Charade"));
-            //enigmas.Add(new Enigma(new ClicRapideEnigmaPanel(), "Clic-rapide"));
-            //enigmas.Add(new Enigma(new DingbatEnigmaPanel(), "Jeu de mot"));
-            //enigmas.Add(new Enigma(new FruitsEnigmaPanel(), "Calcul"));
-            //enigmas.Add(new Enigma(new HazardEnigmaPanel(), "Hazard"));
-            //enigmas.Add(new Enigma(new HiddenCharacterEnigmalPanel(), "Caractère caché"));
-            //enigmas.Add(new Enigma(new LettreHaute(), "Quelle est la plus haute ?"));
-            //enigmas.Add(new Enigma(new MemoireDesChiffresEnigmaPanel(), "Souviens toi"));
-            //enigmas.Add(new Enigma(new MorpionEnigmaPanel(), "Morpion"));
-            //enigmas.Add(new Enigma(new NbrCarresEnigmaPanel(), "Nombre de carrés"));
-            //enigmas.Add(new Enigma(new NfsEnigmaPanel(), "Need For Speed"));
-            //enigmas.Add(new Enigma(new NinePointsEnigmaPanel(), "9 Points"));
-            //enigmas.Add(new Enigma(new OiseauxEnigmaPanel(), "Le plus long mot"));
-            //enigmas.Add(new Enigma(new OpenDoorEnigmaPanel(), "Ouvrez la porte !"));
-            //enigmas.Add(new Enigma(new OuEstLaReponseEnigmaPanel(), "Où est la réponse ?"));
-            //enigmas.Add(new Enigma(new PenduEnigmaPanel(), "Le jeu du pendu"));
-            //enigmas.Add(new Enigma(new PhoqueEnigmaPanel(), "Chop' les poissons"));
-            //enigmas.Add(new Enigma(new RectangleEnigmaPanel(), "Trouve le rectangle"));
-            //enigmas.Add(new Enigma(new ReflexeEnigmaPanel(), "Reflexe"));
-            //enigmas.Add(new Enigma(new SeptDifferencesEnigmaPanel(), "Le jeu des 7 différences"));
-            //enigmas.Add(new Enigma(new SimpleEnigmaPanel(), "C'est simple"));
-            //enigmas.Add(new Enigma(new SimplePuzzleEnigmaPanel(), "Puzzle"));
-            //enigmas.Add(new Enigma(new TapeTaupeEnigmaPanel(), "Tape-taupe"));
-            //enigmas.Add(new Enigma(new TrouverEnigmaPanel(), "Trouver Cristiano"));
-            //enigmas.Add(new Enigma(new ZombieInvasionEnigmaPanel(), "ZombieInvasion"));
+            enigmas.Add(new Enigma(new AppuieReponseEnigmaPanel(), "Appuie sur la réponse"));
+            enigmas.Add(new Enigma(new BeatThemAllEnigmaPanel(), "Beat them all"));
+            enigmas.Add(new Enigma(new BusEnigmaPanel(), "Sens du bus"));
+            enigmas.Add(new Enigma(new CaseVideEnigmaPanel(), "Et ben non"));
+            enigmas.Add(new Enigma(new CharadeEnigmaPanel(), "Charade"));
+            enigmas.Add(new Enigma(new ClicRapideEnigmaPanel(), "Clic-rapide"));
+            enigmas.Add(new Enigma(new DingbatEnigmaPanel(), "Jeu de mot"));
+            enigmas.Add(new Enigma(new FruitsEnigmaPanel(), "Calcul"));
+            enigmas.Add(new Enigma(new HazardEnigmaPanel(), "Hazard"));
+            enigmas.Add(new Enigma(new HiddenCharacterEnigmalPanel(), "Caractère caché"));
+            enigmas.Add(new Enigma(new LettreHaute(), "Quelle est la plus haute ?"));
+            enigmas.Add(new Enigma(new MemoireDesChiffresEnigmaPanel(), "Souviens toi"));
+            enigmas.Add(new Enigma(new MorpionEnigmaPanel(), "Morpion"));
+            enigmas.Add(new Enigma(new NbrCarresEnigmaPanel(), "Nombre de carrés"));
+            enigmas.Add(new Enigma(new NfsEnigmaPanel(), "Need For Speed"));
+            enigmas.Add(new Enigma(new NinePointsEnigmaPanel(), "9 Points"));
+            enigmas.Add(new Enigma(new OiseauxEnigmaPanel(), "Le plus long mot"));
+            enigmas.Add(new Enigma(new OpenDoorEnigmaPanel(), "Ouvrez la porte !"));
+            enigmas.Add(new Enigma(new OuEstLaReponseEnigmaPanel(), "Où est la réponse ?"));
+            enigmas.Add(new Enigma(new PenduEnigmaPanel(), "Le jeu du pendu"));
+            enigmas.Add(new Enigma(new PhoqueEnigmaPanel(), "Chop' les poissons"));
+            enigmas.Add(new Enigma(new RectangleEnigmaPanel(), "Trouve le rectangle"));
+            enigmas.Add(new Enigma(new ReflexeEnigmaPanel(), "Reflexe"));
+            enigmas.Add(new Enigma(new SeptDifferencesEnigmaPanel(), "Le jeu des 7 différences"));
+            enigmas.Add(new Enigma(new SimpleEnigmaPanel(), "C'est simple"));
+            enigmas.Add(new Enigma(new SimplePuzzleEnigmaPanel(), "Puzzle"));
+            enigmas.Add(new Enigma(new TapeTaupeEnigmaPanel(), "Tape-taupe"));
+            enigmas.Add(new Enigma(new TrouverEnigmaPanel(), "Trouver Cristiano"));
+            enigmas.Add(new Enigma(new ZombieInvasionEnigmaPanel(), "ZombieInvasion"));
             enigmas.Add(new Enigma(new PlateformerEnigmaPanel(), "Plateformer"));
-=======
             enigmas.Add(new Enigma(new AppuieReponseEnigmaPanel(), "Appuie sur la réponse"));
             enigmas.Add(new Enigma(new BeatThemAllEnigmaPanel(), "Beat them all"));
             enigmas.Add(new Enigma(new BusEnigmaPanel(), "Sens du bus"));
@@ -93,7 +91,6 @@ namespace Cpln.Enigmos
             enigmas.Add(new Enigma(new TapeTaupeEnigmaPanel(), "Tape-taupe"));
             enigmas.Add(new Enigma(new TrouverEnigmaPanel(), "Trouver Cristiano"));
             enigmas.Add(new Enigma(new ZombieInvasionEnigmaPanel(), "ZombieInvasion"));
->>>>>>> upstream/master
 
             Enigma runEnigma = new Enigma(new RunEnigmaPanel(), "Roux run");
             enigmas.Add(runEnigma);
