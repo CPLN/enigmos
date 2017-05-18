f267b2c90b81348d44efff68722c9cbb443555f5
Rime <vincent.rime@rpn.ch>
2017-05-18 16:23:52 +0200

    couleur du panel avec tout les animations

--- Enigmas/Components/Touche.cs
+++ Enigmas/Components/Touche.cs
@@ -12,31 +12,68 @@ namespace Cpln.Enigmos.Enigmas.Components
 {
     class Touche : MovablePanel
     {
+        private Color couleurPanel;
+        private Color couleurPolice;
+
         public string Nom { get; set; }
 
         public Touche(string nom,int Width, int Height)
         {
-            this.Nom = nom;
-            
+            this.Nom = nom;            
             this.Width = Width ;
             this.Height = Height ;
+
             this.Cursor = Cursors.Arrow;
+            this.couleurPanel = Color.Black;
+            this.couleurPolice = Color.White;
+
             this.Paint += new PaintEventHandler(Dessin);
-            SourisBouge += new MouseEventHandler(Bouge); // evenement quand la souris est sur le panel
+            this.MouseEnter += new EventHandler(SourisEntre);
+            this.MouseLeave += new EventHandler(SourisSort);
+            this.MouseDown += new MouseEventHandler(SourisDown);
+            this.MouseUp += new MouseEventHandler(SourisUp);
+
+            DoubleBuffered = true;
         }
 
 
         private void Dessin(object sender, PaintEventArgs e)
         {
-            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
-            e.Graphics.FillRectangle(Brushes.Black, 10, 0, Width -20, Height);
-            e.Graphics.FillRectangle(Brushes.Black, 0, 10, Width, Height -20);
+            Brush brushPanel = new SolidBrush(couleurPanel);
+            Brush brushPolice = new SolidBrush(couleurPolice);
 
-            e.Graphics.FillEllipse(Brushes.Black , 0, 0, 20, 20);
-            e.Graphics.FillEllipse(Brushes.Black, 0,Height-20, 20, 20);
-            e.Graphics.FillEllipse(Brushes.Black, Width-20, 0, 20, 20);
-            e.Graphics.FillEllipse(Brushes.Black, Width-20, Height-20, 20, 20);
-            e.Graphics.DrawString(Nom, new Font("Arial", 12), Brushes.White, 2, 2);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.FillRectangle(brushPanel, 10, 0, Width -20, Height);
+            e.Graphics.FillRectangle(brushPanel, 0, 10, Width, Height -20);
+
+            e.Graphics.FillEllipse(brushPanel, 0, 0, 20, 20);
+            e.Graphics.FillEllipse(brushPanel, 0,Height-20, 20, 20);
+            e.Graphics.FillEllipse(brushPanel, Width-20, 0, 20, 20);
+            e.Graphics.FillEllipse(brushPanel, Width-20, Height-20, 20, 20);
+            e.Graphics.DrawString(Nom, new Font("Arial", 12), brushPolice, 2, 2);
+        }
+        private void SourisEntre(object sender, EventArgs e)
+        {
+            this.couleurPanel = Color.Gray;
+            Invalidate();
+        }
+
+        private void SourisSort(object sender, EventArgs e)
+        {
+            this.couleurPanel = Color.Black;
+            Invalidate();
+        }
+        private void SourisDown(object sender, MouseEventArgs e)
+        {
+            this.couleurPolice = Color.Black;
+            this.couleurPanel = Color.LightGray;
+            Invalidate();
+        }
+        private void SourisUp(object sender, MouseEventArgs e)
+        {
+            this.couleurPolice = Color.White;
+            this.couleurPanel = Color.Gray;
+            Invalidate();
         }
     }
 }
