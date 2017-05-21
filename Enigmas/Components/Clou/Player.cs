9af568b50d7232ef20b29ed26f18ed1607fe1bad
danydacosta <danydc@comac.ch>
2017-05-21 11:21:24 +0200

    Création de la classe Player

---
+++ Enigmas/Components/Clou/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cpln.Enigmos.Enigmas.Components.Clou
+{
+    /// <summary>
+    /// Classe représentant un joueur du jeu du clou
+    /// </summary>
+    class Player
+    {
+        public Player()
+        {
+
+        }
+    }
+}
