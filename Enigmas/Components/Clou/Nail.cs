e2e312aa9b405c0a2ba561cb78c7bd8f15ba8917
danydacosta <danydc@comac.ch>
2017-05-21 10:47:14 +0200

    Ajout de l'image par défaut du clou

--- Enigmas/Components/Clou/Nail.cs
+++ Enigmas/Components/Clou/Nail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,18 @@ namespace Cpln.Enigmos.Enigmas.Components.Clou
     /// </summary>
     class Nail : PictureBox
     {
+        /// <summary>
+        /// Constructeur : Définition/instanciation des valeurs par défaut.
+        /// </summary>
         public Nail()
         {
+            #region Pbx propriétés
+            //Définition de l'image source
+            BackgroundImage = Properties.Resources.nailResized;
 
+            //Définition de la taille de l'image
+            Size = new Size(101, 356);
+            #endregion
         }
     }
 }
