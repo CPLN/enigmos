0ac9d1d1d67a22a80e9539ddd1e518c769b0af1d
danydacosta <danydc@comac.ch>
2017-05-21 12:10:39 +0200

    Tests des classes

--- EnigmaReferencer.cs
+++ EnigmaReferencer.cs
@@ -17,7 +17,7 @@ namespace Cpln.Enigmos
         /// <returns>L'énigme à afficher</returns>
         public static Enigma DebugEnigma()
         {
-            return new Enigma(new SwitchesEnigmaPanel(3, "LUMINEUX"), "Allumez tout");
+            return new Enigma(new ClouEnigmaPanel(), "Jeu du clou");
         }
 
         /// <summary>
--- Enigmas/ClouEnigmaPanel.cs
+++ Enigmas/ClouEnigmaPanel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Cpln.Enigmos.Enigmas.Components.Clou;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Cpln.Enigmos.Enigmas
 {
@@ -11,6 +14,47 @@ namespace Cpln.Enigmos.Enigmas
     /// </summary>
     class ClouEnigmaPanel : EnigmaPanel
     {
+        EnergyBar bar = new EnergyBar();
+        Nail nail = new Nail();
+        IA ia = new IA();
+        Label test = new Label();
 
+        public ClouEnigmaPanel()
+        {
+            test.Location = new Point(150, 150);
+            //test.
+            nail.Location = new Point(44, 0);
+            Controls.Add(bar);
+            Controls.Add(nail);
+            Controls.Add(test);
+        }
+
+        public override void PressKey(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                //player
+                nail.Down(bar.CaptureCursorPower());
+
+                if (nail.Location.Y >= 356)
+                {
+                    MessageBox.Show("tu a gagné !");
+                }
+
+                System.Threading.Thread.Sleep(1000);
+
+                //ia
+                nail.Down(ia.CalculateBlowPower(nail));
+
+
+                //qui a gagne ?
+                if (nail.Location.Y >= 356)
+                {
+                    MessageBox.Show("l'ia a gagné !");
+                }
+
+                bar.StartCursor();
+            }
+        }
     }
 }
