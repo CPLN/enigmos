0f14a12b0309cbe613896795531e7100cd1762d0
danydacosta <danydc@comac.ch>
2017-05-21 11:29:15 +0200

    Création de la classe IA

---
+++ Enigmas/Components/Clou/IA.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cpln.Enigmos.Enigmas.Components.Clou
+{
+    /// <summary>
+    /// Classe représentant l'IA qui va jouer contre le joueur humain.
+    /// </summary>
+    class IA : Player
+    {
+        /// <summary>
+        /// Constructeur : Définition/instanciation des valeurs par défaut.
+        /// </summary>
+        public IA()
+        {
+
+        }
+    }
+}
