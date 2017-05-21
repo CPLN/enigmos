0679d995b9bdcd767960d7d282907d685c968329
danydacosta <danydc@comac.ch>
2017-05-21 10:28:36 +0200

    Création de la classe nail

---
+++ Enigmas/Components/Clou/Nail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cpln.Enigmos.Enigmas.Components.Clou
+{
+    /// <summary>
+    /// Classe représentant un clou
+    /// </summary>
+    class Nail : PictureBox
+    {
+        public Nail()
+        {
+
+        }
+    }
+}
