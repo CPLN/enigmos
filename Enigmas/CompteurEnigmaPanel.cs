2f413ca8d28d4e9a86b78183d077330a019c5479
Wasem <aurelie.wasem@rpn.ch>
2017-05-22 12:13:03 +0200

    Debut de la mise en place du timer

--- Enigmas/CompteurEnigmaPanel.cs
+++ Enigmas/CompteurEnigmaPanel.cs
@@ -10,21 +10,45 @@ namespace Cpln.Enigmos.Enigmas
 {
     public class CompteurEnigmaPanel:EnigmaPanel
     {
+        private Timer tStart = new Timer();
+        private Label lblTemps = new Label();
         public CompteurEnigmaPanel()
         {
             Button btnStart = new Button();
-            Label lblTemps = new Label();
 
-            //Mise en place du boutton et du label
+            //Mise en place du bouton et du label
             lblTemps.Size = new Size(200, 200);
             lblTemps.Location = new Point(300, 200);
             lblTemps.BackColor = Color.Red;
+            lblTemps.Font = new Font("Arial", 30);
             Controls.Add(lblTemps);
             btnStart.Size = new Size(200, 50);
             btnStart.Location = new Point(300, 400);
             btnStart.Font = new Font("Arial", 30);
             btnStart.Text = "Start";
             Controls.Add(btnStart);
+            btnStart.Click+=new EventHandler(Debut);
+            //Paramètre du Timer
+            tStart.Interval=1000;
+        }
+
+        /// <summary>
+        /// Lancement du compteur
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Debut(object sender, EventArgs e)
+        {
+            tStart.Start();
+        }
+
+
+        public override void PressKey(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                tStart.Stop();
+            }
         }
     }
 }
