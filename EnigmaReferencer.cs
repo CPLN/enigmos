8594c9fcd8516f4d45a0ec070ce150320b37c907
Steeve Droz <steeve.droz@rpn.ch>
2017-06-09 10:57:30 +0200

    Mise en forme

--- EnigmaReferencer.cs
+++ EnigmaReferencer.cs
@@ -17,7 +17,7 @@ namespace Cpln.Enigmos
         /// <returns>L'énigme à afficher</returns>
         public static Enigma DebugEnigma()
         {
-            return new Enigma(new PileOuFaceEnigmaPanel(), "Pile ou face");
+            return new Enigma(new Pendu2EnigmaPanel(), "Pendu 2");
         }
 
         /// <summary>
